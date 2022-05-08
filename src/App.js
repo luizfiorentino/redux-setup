8e9d635c252bc63a3bb8026616ff7898b85dc503
luizfiorentino <lgfiorentino@gmail.com>
2022-05-08 09:49:56 +0200

    dispatch action- sending data to the store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deposit } from "./store/balance/slice";
 
 function App() {
+  const dispatch = useDispatch();
   const [balance, setBalance] = useState(0);
 
   return (
@@ -8,7 +11,8 @@ function App() {
       <p>Balance: {balance}$</p>
       <button
         onClick={() => {
-          setBalance(balance + 10);
+          /*setBalance(balance + 10);*/
+          dispatch(deposit(10));
         }}
       >
         Deposit 10$
--- src/store/balance/slice.js
+++ src/store/balance/slice.js
@@ -7,12 +7,14 @@ const initialState = {
 export const balanceSlice = createSlice({
   name: "balance",
   initialState,
-  reducers: {},
+  reducers: {
+    deposit: (state) => {},
+  },
 });
 
 // Action creators are generated for each case reducer function
 // as we add cases to our reducer we will also export the corresponding acions
 
-export const {} = balanceSlice.actions;
+export const { deposit } = balanceSlice.actions;
 
 export default balanceSlice.reducer;
