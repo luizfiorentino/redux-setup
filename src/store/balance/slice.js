b2cc83827946a5ea593190e865eb90022a72531a
luizfiorentino <lgfiorentino@gmail.com>
2022-05-08 10:21:30 +0200

    feat/app component successfully using redux

--- src/store/balance/slice.js
+++ src/store/balance/slice.js
@@ -8,13 +8,16 @@ export const balanceSlice = createSlice({
   name: "balance",
   initialState,
   reducers: {
-    deposit: (state) => {},
+    // "deposit is the key of the reducer"
+    deposit: (state, action) => {
+      state.amount = state.amount + action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
 // as we add cases to our reducer we will also export the corresponding acions
-
+// Add the newly created key to our export statement
 export const { deposit } = balanceSlice.actions;
 
 export default balanceSlice.reducer;
